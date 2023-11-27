52bf58c5995ec2a5c08cac52c05ab0fe86a657ab
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-11-27 16:54:37 +0200

    3d island

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,8 +1,12 @@
+import { Html } from '@react-three/drei';
+
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-20 h-20 border-t-blue-500 rounded-full animate-spin" />
-    </div>
+    <Html>
+      <div className="flex items-center justify-center">
+        <div className="w-20 h-20 border-t-blue-500 rounded-full animate-spin" />
+      </div>
+    </Html>
   );
 };
 
